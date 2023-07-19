a1885f7e4951b935e5bfa1d2c7328979f3623b24
buckp4012 <127550556+buckp4012@users.noreply.github.com>
2023-07-19 16:00:52 -0500

    Update SAGD_DB.py

--- SAGD_DB.py
+++ SAGD_DB.py
@@ -15,15 +15,16 @@ from datetime import datetime
 
 
 #Load data from CSV file or pandas dataframe
-df = pd.read_excel('SAGD Dashboard V2.xlsx')
+df = pd.read_excel('SAGD Dashboard V2-new.xlsx')
 
 app = dash.Dash(__name__)
 server = app.server
-data = df.loc[df['R/NR/Waiting'].ne('Waiting') & df['R/NR/Waiting'].notna()]
+data = df.loc[df['R/NR/Waiting'].notna()]
 
 data=data.groupby('CUSTOMER').filter(lambda x: (x['R/NR/Waiting'] == 'NR').sum() >= 1)
 
 customers = data['CUSTOMER'].unique()
+
 
 # convert the customer names to the required format for dcc.Dropdown options
 options = [{'label': 'All Customers', 'value': 'all'}] + \
@@ -60,7 +61,9 @@ app.layout = html.Div(
         html.Tr([
             html.Td('Version 3.0: Redesigning all the barstock for zero-leak porting, additional flow paths added along with parallel check valves'),]),
         html.Tr([
-            html.Td('Version 3.0- New screen: Units with Seal Version 3.0 and a new screen intake'),])
+            html.Td('Version 3.0- New screen: Units with Seal Version 3.0 and a new screen intake'),]),
+html.Tr([
+            html.Td('Version 3.5- Removed the parallel check valves and replaced them with a single new (20 psi) cartridge valve , plugged off the second check valve port, removed three breather pipes in the head and replaced them with plugs'),])
     ], style={"margin": "auto","margin-top": "0"}),
         dcc.Dropdown(
             id='customer-dropdown',
@@ -78,7 +81,8 @@ app.layout = html.Div(
                 {'label': 'Version 2', 'value': 'Version 2'},
                 {'label': 'Version 2.5', 'value': 'Version 2.5'},
                 {'label': 'Version 3.0', 'value': 'Version 3.0'},
-                {'label': 'Version 3.0- New screen', 'value': 'Version 3.0- New screen'}
+                {'label': 'Version 3.0- New screen', 'value': 'Version 3.0- New screen'},
+                {'label': 'Version 3.5', 'value': 'Version 3.5'}
             ],
             value='all'
         ),
@@ -122,6 +126,8 @@ def update_pie_charts(selected_seal_version, selected_customer):
             filtered_data[(filtered_data['SEAL'] == 'Version 3.0') & (filtered_data['R/NR/Waiting'] == 'NR')])
         count_ver3_ns = len(filtered_data[(filtered_data['SEAL'] == 'Version 3.0- New screen') & (
                 filtered_data['R/NR/Waiting'] == 'NR')])
+        count_ver3_5 = len(filtered_data[(filtered_data['SEAL'] == 'Version 3.5') & (
+                filtered_data['R/NR/Waiting'] == 'NR')])
         Run_total = len(filtered_data[filtered_data['R/NR/Waiting'] == 'R'])
         Run_ver1 = len(filtered_data[(filtered_data['SEAL'] == 'Version 1') & (filtered_data['R/NR/Waiting'] == 'R')])
         Run_ver2 = len(filtered_data[(filtered_data['SEAL'] == 'Version 2') & (filtered_data['R/NR/Waiting'] == 'R')])
@@ -130,7 +136,8 @@ def update_pie_charts(selected_seal_version, selected_customer):
         Run_ver3 = len(filtered_data[(filtered_data['SEAL'] == 'Version 3.0') & (filtered_data['R/NR/Waiting'] == 'R')])
         Run_ver3_ns = len(filtered_data[(filtered_data['SEAL'] == 'Version 3.0- New screen') & (
                 filtered_data['R/NR/Waiting'] == 'R')])
-
+        Run_ver3_5 = len(
+            filtered_data[(filtered_data['SEAL'] == 'Version 3.5') & (filtered_data['R/NR/Waiting'] == 'R')])
         days_total = filtered_data[filtered_data['R/NR/Waiting'] == 'NR']['RUNTIME (Excel Calculated)'].sum()
         days_ver1 = filtered_data[(filtered_data['SEAL'] == 'Version 1') & (filtered_data['R/NR/Waiting'] == 'NR')][
             'RUNTIME (Excel Calculated)'].sum()
@@ -142,7 +149,8 @@ def update_pie_charts(selected_seal_version, selected_customer):
             'RUNTIME (Excel Calculated)'].sum()
         days_ver3_ns =filtered_data[(filtered_data['SEAL'] == 'Version 3.0- New screen') & (filtered_data['R/NR/Waiting'] == 'NR')][
             'RUNTIME (Excel Calculated)'].sum()
-
+        days_ver3_5 = filtered_data[(filtered_data['SEAL'] == 'Version 3.5') & (filtered_data['R/NR/Waiting'] == 'NR')][
+            'RUNTIME (Excel Calculated)'].sum()
         subset1 = filtered_data.loc[filtered_data['SEAL'] == 'Version 1']
         avg1 = subset1['RUNTIME (Excel Calculated)'].mean()
         max_run1 = subset1['RUNTIME (Excel Calculated)'].max()
@@ -173,6 +181,12 @@ def update_pie_charts(selected_seal_version, selected_customer):
         install3_ns = subset3_ns['RUNTIME (Excel Calculated)'].count()
         subset3_ns['INSTALL'] = pd.to_datetime(subset3_ns['INSTALL'])
         filtered_data1 = filtered_data
+
+        subset3_5 = filtered_data.loc[filtered_data['SEAL'] == 'Version 3.5']
+        avg3_5 = subset3_5['RUNTIME (Excel Calculated)'].mean()
+        max_run3_5 = subset3_5['RUNTIME (Excel Calculated)'].max()
+        install3_5 = subset3_5['RUNTIME (Excel Calculated)'].count()
+        subset3_5['INSTALL'] = pd.to_datetime(subset3_5['INSTALL'])
         
 
         if selected_seal_version != 'all':
@@ -189,6 +203,8 @@ def update_pie_charts(selected_seal_version, selected_customer):
         count_ver2_5 = len(filtered_data[(filtered_data['SEAL'] == 'Version 2.5') & (filtered_data['R/NR/Waiting'] == 'NR')])
         count_ver3 = len(filtered_data[(filtered_data['SEAL'] == 'Version 3.0') & (filtered_data['R/NR/Waiting'] == 'NR')])
         count_ver3_ns = len(filtered_data[(filtered_data['SEAL'] == 'Version 3.0- New screen') & (filtered_data['R/NR/Waiting'] == 'NR')])
+        count_ver3_5 = len(
+            filtered_data[(filtered_data['SEAL'] == 'Version 3.5') & (filtered_data['R/NR/Waiting'] == 'NR')])
         Run_total = len(filtered_data[filtered_data['R/NR/Waiting'] == 'R'])
         Run_ver1 = len(filtered_data[(filtered_data['SEAL'] == 'Version 1') & (filtered_data['R/NR/Waiting'] == 'R')])
         Run_ver2 = len(filtered_data[(filtered_data['SEAL'] == 'Version 2') & (filtered_data['R/NR/Waiting'] == 'R')])
@@ -197,7 +213,8 @@ def update_pie_charts(selected_seal_version, selected_customer):
         Run_ver3 = len(filtered_data[(filtered_data['SEAL'] == 'Version 3.0') & (filtered_data['R/NR/Waiting'] == 'R')])
         Run_ver3_ns = len(filtered_data[(filtered_data['SEAL'] == 'Version 3.0- New screen') & (
                 filtered_data['R/NR/Waiting'] == 'R')])
-
+        Run_ver3_5 = len(
+            filtered_data[(filtered_data['SEAL'] == 'Version 3.5') & (filtered_data['R/NR/Waiting'] == 'R')])
         days_total = filtered_data[filtered_data['R/NR/Waiting'] == 'NR']['RUNTIME (Excel Calculated)'].sum()
         days_ver1 = filtered_data[(filtered_data['SEAL'] == 'Version 1') & (filtered_data['R/NR/Waiting'] == 'NR')][
             'RUNTIME (Excel Calculated)'].sum()
@@ -209,7 +226,8 @@ def update_pie_charts(selected_seal_version, selected_customer):
             'RUNTIME (Excel Calculated)'].sum()
         days_ver3_ns = filtered_data[(filtered_data['SEAL'] == 'Version 3.0- New screen') & (filtered_data['R/NR/Waiting'] == 'NR')][
             'RUNTIME (Excel Calculated)'].sum()
-
+        days_ver3_5 = filtered_data[(filtered_data['SEAL'] == 'Version 3.5') & (filtered_data['R/NR/Waiting'] == 'NR')][
+            'RUNTIME (Excel Calculated)'].sum()
         subset1 = filtered_data.loc[filtered_data['SEAL'] == 'Version 1']
         avg1 = subset1['RUNTIME (Excel Calculated)'].mean()
         max_run1 = subset1['RUNTIME (Excel Calculated)'].max()
@@ -234,6 +252,11 @@ def update_pie_charts(selected_seal_version, selected_customer):
         avg3_ns = subset3_ns['RUNTIME (Excel Calculated)'].mean()
         max_run3_ns = subset3_ns['RUNTIME (Excel Calculated)'].max()
         install3_ns = subset3_ns['RUNTIME (Excel Calculated)'].count()
+        
+        subset3_5 = filtered_data.loc[filtered_data['SEAL'] == 'Version 3.5']
+        avg3_5 = subset3_5['RUNTIME (Excel Calculated)'].mean()
+        max_run3_5 = subset3_5['RUNTIME (Excel Calculated)'].max()
+        install3_5 = subset3_5['RUNTIME (Excel Calculated)'].count()
         
         if selected_seal_version != 'all':
             filtered_data1 = filtered_data[filtered_data['SEAL'] == selected_seal_version]
@@ -275,6 +298,10 @@ def update_pie_charts(selected_seal_version, selected_customer):
         mttf_ver3_ns = "N/A"
     else:
         mttf_ver3_ns = round(days_ver3_ns / count_ver3_ns)
+    if days_ver3_5==0 or count_ver3_5==0:
+        mttf_ver3_5="N/A"
+    else:
+        mttf_ver3_5 = round(days_ver3_5 / count_ver3_5)
     if install_total==0:
         avg_new="N/A"
         max_run="N/A"
@@ -329,15 +356,24 @@ def update_pie_charts(selected_seal_version, selected_customer):
         first_install3_ns = subset3_ns['INSTALL'].min()
         first_install3_ns_str = first_install3_ns.strftime('%m/%d/%Y')
 
+    if install3_5==0:
+        avg3_5_new="N/A"
+        max_run3_5="N/A"
+        Run_ver3_5="N/A"
+        first_install3_5_str = "N/A"
+    else:
+        avg3_5_new=round(avg3_5)
+        first_install3_5 = subset3_5['INSTALL'].min()
+        first_install3_5_str = first_install3_5.strftime('%m/%d/%Y')
     table_data = pd.DataFrame(
         {'Seal Version': ['All Versions', 'Version 1', 'Version 2', 'Version 2.5', 'Version 3',
-                          'Version 3.0- New screen'],
-         'First Install': [first_install_str, first_install1_str, first_install2_str, first_install2_5_str, first_install3_str, first_install3_ns_str],
-         'Average Runtime': [avg_new, avg1_new, avg2_new, avg2_5_new, avg3_new, avg3_ns_new],
-         'Installs': [install_total, install1, install2, install2_5, install3, install3_ns],
-         'Actively Running': [Run_total, Run_ver1, Run_ver2, Run_ver2_5, Run_ver3, Run_ver3_ns],
-         'MTTF (days)': [mttf_total, mttf_ver1, mttf_ver2, mttf_ver2_5, mttf_ver3, mttf_ver3_ns],
-         'Max Runtime': [max_run, max_run1, max_run2, max_run2_5, max_run3, max_run3_ns]
+                          'Version 3.0- New screen', 'Version 3.5'],
+         'First Install': [first_install_str, first_install1_str, first_install2_str, first_install2_5_str, first_install3_str, first_install3_ns_str,first_install3_5_str],
+         'Average Runtime': [avg_new, avg1_new, avg2_new, avg2_5_new, avg3_new, avg3_ns_new,avg3_5_new],
+         'Installs': [install_total, install1, install2, install2_5, install3, install3_ns,install3_5],
+         'Actively Running': [Run_total, Run_ver1, Run_ver2, Run_ver2_5, Run_ver3, Run_ver3_ns,Run_ver3_5],
+         'MTTF (days)': [mttf_total, mttf_ver1, mttf_ver2, mttf_ver2_5, mttf_ver3, mttf_ver3_ns,mttf_ver3_5],
+         'Max Runtime': [max_run, max_run1, max_run2, max_run2_5, max_run3, max_run3_ns,max_run3_5]
          })
     table_fig = go.Figure(data=[go.Table(
         header=dict(values=list(table_data.columns),
